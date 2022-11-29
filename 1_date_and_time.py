7be222546c47eba40cd6ce5314e1a69edf8e4de5
Nastya <annika-purtova@yandex.ru>
2022-11-29 19:41:08 +0300

    Updated

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -15,21 +15,16 @@ from datetime import date, timedelta
 def print_days():
     dt_now = datetime.now()
 
-    # Сегодня
     print(dt_now.strftime('%d.%m.%Y %H:%M'))
 
-    # Вчера
     yesterday = datetime.now() - timedelta(days=1)
     print(yesterday.strftime('%d.%m.%Y %H:%M'))
 
-    # 30 дней назад
     month_ago = datetime.now() - timedelta(days=30)
     print(month_ago.strftime('%d.%m.%Y %H:%M'))
 
 
 def str_2_datetime(date_string):
-    # Превратите строку "01/01/25 12:10:03.234567" в объект datetime
-
     date_str = '01/01/25 12:10:03.234567'
     date_dt = datetime.strptime(date_str, '%m/%d/%y %H:%M:%S.%f')
     return date_dt
--- 2_files.py
+++ 2_files.py
@@ -15,14 +15,11 @@ def main():
     with open('referat.txt', 'r', encoding='utf-8') as f:
         content = f.read()
 
-    # Прочитайте содержимое файла в переменную, подсчитайте длину получившейся строки
     str_len = len(content)
     print(str_len)
 
-    # Подсчитайте количество слов в тексте
     print(len(content.split()))
 
-    # Замените точки в тексте на восклицательные знаки
     x = ''
     for i in content:
         if i not in ('.','?'):
@@ -31,8 +28,6 @@ def main():
             print((x+i).strip().replace('.','!'))
             x = ''
 
-
-    # Сохраните результат в файл referat2.txt
     filename = 'referat2.txt'
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
