8599cd9b27cb1e2dc7525b52fe9cd4c0f4602287
Nastya <annika-purtova@yandex.ru>
2022-11-29 15:07:58 +0300

    Solutions for exercises

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -8,20 +8,31 @@
 
 """
 
+from datetime import datetime
+from datetime import date, timedelta
+
+
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dt_now = datetime.now()
+
+    # Сегодня
+    print(dt_now.strftime('%d.%m.%Y %H:%M'))
+
+    # Вчера
+    yesterday = datetime.now() - timedelta(days=1)
+    print(yesterday.strftime('%d.%m.%Y %H:%M'))
+
+    # 30 дней назад
+    month_ago = datetime.now() - timedelta(days=30)
+    print(month_ago.strftime('%d.%m.%Y %H:%M'))
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    # Превратите строку "01/01/25 12:10:03.234567" в объект datetime
+
+    date_str = '01/01/25 12:10:03.234567'
+    date_dt = datetime.strptime(date_str, '%m/%d/%y %H:%M:%S.%f')
+    return date_dt
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -12,11 +12,31 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    # Прочитайте содержимое файла в переменную, подсчитайте длину получившейся строки
+    str_len = len(content)
+    print(str_len)
+
+    # Подсчитайте количество слов в тексте
+    print(len(content.split()))
+
+    # Замените точки в тексте на восклицательные знаки
+    x = ''
+    for i in content:
+        if i not in ('.','?'):
+            x += i
+        else:
+            print((x+i).strip().replace('.','!'))
+            x = ''
+
+
+    # Сохраните результат в файл referat2.txt
+    filename = 'referat2.txt'
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+        
 
 if __name__ == "__main__":
     main()
